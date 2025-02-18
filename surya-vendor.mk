089439e7751f370a2e4883b79a95d88d299e8b74
shoya0x00 <shoya0x00@gmail.com>
2025-02-18 07:54:41 +0000

    surya: Bringback libmisoundfx

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -322,6 +322,7 @@ PRODUCT_PACKAGES += \
     libsnsapi \
     libvpphvx \
     libvpplibrary \
+    libmisoundfx \
     vendor.qti.hardware.dsp@1.0 \
     vendor.qti.hardware.vpp@1.1 \
     vendor.qti.hardware.vpp@1.2 \
